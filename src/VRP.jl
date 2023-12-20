e0136e8a8f64045e0bb905fe9151e61e51c99710
anmol1104 <anmpahwa@ucdavis.edu>
2023-12-19 20:35:09 -0800

    update VRP.jl

--- src/VRP.jl
+++ src/VRP.jl
@@ -1,5 +1,36 @@
 module VRP
 
-# Write your package code here.
+using Clustering
+using CSV
+using DataFrames
+using Distributions
+using ElasticArrays
+using OffsetArrays
+using Plots
+using ProgressMeter
+using Random
+using StatsBase
+
+ElasticArrays.ElasticMatrix(A::OffsetMatrix) = OffsetMatrix(ElasticArray(A), A.offsets)
+Base.append!(A::OffsetMatrix, items) = (append!(A.parent, items); A)
+
+global φᵉ = false::Bool
+
+include("sample.jl")
+include("datastructure.jl")
+include("functions.jl")
+include("initialize.jl")
+include("operations.jl")
+include("remove.jl")
+include("insert.jl")
+include("localsearch.jl")
+include("parameters.jl")
+include("ALNS.jl")
+include("visualize.jl")
+
+export  build, cluster, initialize, 
+        vectorize, f, isfeasible, 
+        ALNSparameters, ALNS, 
+        visualize, animate
 
 end
