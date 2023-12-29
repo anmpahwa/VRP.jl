e3bf619255452869f0c3376d369f962709db674c
anmol1104 <anmpahwa@ucdavis.edu>
2023-12-29 01:50:28 -0800

    minor change

--- src/localsearch.jl
+++ src/localsearch.jl
@@ -100,8 +100,7 @@ function intermove!(rng::AbstractRNG, k̅::Int, s::Solution)
         r₁ = cᵈ.r
         if isdormant(r₁) continue end
         # Step 2.2: Select a random route
-        m  = sample(rng, φᵉ::Bool ? [:q, :l, :t] : [:q, :l])
-        Wʳ = [isdormant(r₂) || isequal(r₁, r₂) ? 0. : relatedness(m, r₁, r₂, s) for r₂ ∈ R]
+        Wʳ = [isdormant(r₂) || isequal(r₁, r₂) ? 0. : 1. for r₂ ∈ R]
         r₂ = sample(rng, R, Weights(Wʳ))
         if isdormant(r₂) continue end
         # Step 2.3: Remove this node from its position between tail node nᵗ and head node nʰ
