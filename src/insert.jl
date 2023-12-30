9c8d862e585b5f1cbc0978b888e26d0efb14588f
anmol1104 <anmpahwa@ucdavis.edu>
2023-12-29 23:27:57 -0800

    update insert.jl

--- src/insert.jl
+++ src/insert.jl
@@ -21,28 +21,28 @@ insert!(s::Solution, method::Symbol) = insert!(Random.GLOBAL_RNG, s, method)
 """
     best!(rng::AbstractRNG, s::Solution)
 
-Returns solution `s` after inserting randomly selected customer node 
-at its best position until all open nodes have been inserted to the 
-solution.
+Returns solution `s` after inserting randomly selected delivery node 
+and its associated pickup node at their best position until all open 
+delivery nodes have been inserted to the solution.
 """
 function best!(rng::AbstractRNG, s::Solution)
     # Step 1: Initialize
     preinsert!(s)
     D = s.D
     C = s.C
-    R = [r for d ∈ D for v ∈ d.V for r ∈ v.R if isactive(r)]
-    L = [c for c ∈ C if isopen(c) && ispickup(c)]
+    R = [r for d ∈ D for v ∈ d.V for r ∈ v.R]
+    L = [c for c ∈ C if isopen(c) && isdelivery(c)]
     I = eachindex(L)
     J = eachindex(R)
-    W = ones(Int, I)                        # W[j]  : selection weight for customer node L[i]
-    X = ElasticMatrix(fill(Inf, (I,J)))     # X[i,j]: insertion cost of customer node L[i] at best position in route R[j]
-    P = ElasticMatrix(fill(((0, 0), (0, 0)), (I,J)))  # P[i,j]: best insertion postion of customer node L[i] in route R[j]
-    # Step 2: Iterate until all open customer nodes have been inserted into the route
+    W = ones(Int, I)                                    # W[j]  : selection weight for delivery node L[i]
+    X = ElasticMatrix(fill(Inf, (I,J)))                 # X[i,j]: insertion cost of delivery node L[i] and its associated pickup node at their best position in route R[j]
+    P = ElasticMatrix(fill(((0, 0), (0, 0)), (I,J)))    # P[i,j]: best insertion postion of associated pickup node and the delivery node L[i] in route R[j]
+    # Step 2: Iterate until all open delivery nodes have been inserted into the route
     for _ ∈ I
-        # Step 2.1: Randomly select an open customer nodes and iterate through all possible insertion positions in each route
-        z = f(s)
-        i = sample(rng, I, Weights(W))
-        c = L[i]
+        # Step 2.1: Randomly select an open delivery node (and the associated pickup node) and iterate through all possible insertion positions in each route
+        z  = f(s)
+        i  = sample(rng, I, Weights(W))
+        c  = L[i]
         cᵖ = isdelivery(c) ? s.C[c.jⁿ] : s.C[c.iⁿ] 
         cᵈ = isdelivery(c) ? s.C[c.iⁿ] : s.C[c.jⁿ]
         for (j,r) ∈ pairs(R)
@@ -52,33 +52,38 @@ function best!(rng::AbstractRNG, s::Solution)
             nᵖᵗ = d
             nᵖʰ = nᵖˢ
             while true
+                # Step 2.1.1: Insert associated pickup node cᵖ between tail node nᵖᵗ and head node nᵖʰ in route r
                 insertnode!(cᵖ, nᵖᵗ, nᵖʰ, r, s)
                 nᵈˢ = isopt(r) ? C[r.iˢ] : D[r.iˢ]
                 nᵈᵉ = isopt(r) ? C[r.iᵉ] : D[r.iᵉ]
                 nᵈᵗ = d
                 nᵈʰ = nᵈˢ
                 while true
+                    # Step 2.1.1.1: Insert delivery node cᵈ between tail node nᵈᵗ and head node nᵈʰ in route r
                     insertnode!(cᵈ, nᵈᵗ, nᵈʰ, r, s)
+                    # Step 2.1.1.2: Compute the insertion cost
                     z′ = f(s)
                     Δ  = z′ - z
+                    # Step 2.1.1.3: Revise least insertion cost in route r and the corresponding best insertion position in route r
                     if Δ < X[i,j] X[i,j], P[i,j] = Δ, ((nᵖᵗ.iⁿ, nᵖʰ.iⁿ), (nᵈᵗ.iⁿ, nᵈʰ.iⁿ)) end
-                    # Step 2.1.4: Remove customer node c from its position between tail node nᵗ and head node nʰ
+                    # Step 2.1.1.4: Remove delivery node cᵈ from its position between tail node nᵈᵗ and head node nᵈʰ in route r
                     removenode!(cᵈ, nᵈᵗ, nᵈʰ, r, s)
                     if isequal(nᵈᵗ, nᵈᵉ) break end
                     nᵈᵗ = nᵈʰ
                     nᵈʰ = isequal(r.iᵉ, nᵈᵗ.iⁿ) ? D[nᵈᵗ.iʰ] : C[nᵈᵗ.iʰ]
                 end
+                # Step 2.1.2: Remove associated pickup node cᵖ between tail node nᵖᵗ and head node nᵖʰ in route r
                 removenode!(cᵖ, nᵖᵗ, nᵖʰ, r, s)
                 if isequal(nᵖᵗ, nᵖᵉ) break end
                 nᵖᵗ = nᵖʰ
                 nᵖʰ = isequal(r.iᵉ, nᵖᵗ.iⁿ) ? D[nᵖᵗ.iʰ] : C[nᵖᵗ.iʰ]
             end
         end
-        # Step 2.2: Insert the customer node at its best position
-        j  = argmin(X[i,:])
-        r  = R[j]
-        d  = s.D[r.iᵈ]
-        v  = d.V[r.iᵛ]
+        # Step 2.2: Insert the delivery node and the associated pickup node at their best position
+        j = argmin(X[i,:])
+        r = R[j]
+        d = s.D[r.iᵈ]
+        v = d.V[r.iᵛ]
         iᵖᵗ = P[i,j][1][1]
         iᵖʰ = P[i,j][1][2]
         iᵈᵗ = P[i,j][2][1]
@@ -91,6 +96,8 @@ function best!(rng::AbstractRNG, s::Solution)
         insertnode!(cᵈ, nᵈᵗ, nᵈʰ, r, s)
         # Step 2.3: Revise vectors appropriately
         W[i] = 0
+        X[i,:] .= Inf
+        P[i,:] .= (((0 ,0), (0, 0)), )
         # Step 2.4: Update solution appropriately 
         if addroute(r, s)
             r = Route(v, d)
@@ -119,26 +126,28 @@ end
 """
     greedy!(rng::AbstractRNG, s::Solution; mode::Symbol)
 
-Returns solution `s` after iteratively inserting customer nodes with least 
-insertion cost until all open customer nodes have been added to the solution. 
-Available modes include `:pcs` (precise estimation of insertion cost) and 
-`:ptb` (perturbed estimation of insertion cost).
+Returns solution `s` after iteratively inserting delivery node 
+and its associated pickup node with least insertion cost until all 
+open delivery nodes have been added to the solution. 
+Available modes include `:pcs` (precise estimation of insertion cost) 
+and `:ptb` (perturbed estimation of insertion cost).
 """
-function greedy!(rng::AbstractRNG, s::Solution; mode::Symbol)
+function greedy!(rng::AbstractRNG, s::Solution, mode::Symbol)
     # Step 1: Initialize
     preinsert!(s)
     D = s.D
     C = s.C
     φ = isequal(mode,  :ptb)
-    R = [r for d ∈ D for v ∈ d.V for r ∈ v.R if isactive(r)]
-    L = [c for c ∈ C if isopen(c) && ispickup(c)]
+    R = [r for d ∈ D for v ∈ d.V for r ∈ v.R]
+    L = [c for c ∈ C if isopen(c) && isdelivery(c)]
     I = eachindex(L)
     J = eachindex(R)
-    X = ElasticMatrix(fill(Inf, (I,J)))     # X[i,j]: insertion cost of customer node L[i] at best position in route R[j]
-    P = ElasticMatrix(fill(((0, 0), (0, 0)), (I,J)))  # P[i,j]: best insertion postion of customer node L[i] in route R[j]
-    ϕ = ones(Int, J)                        # ϕ[j]  : binary weight for route R[j]
-    # Step 2: Iterate until all open customer nodes have been inserted into the route
+    X = ElasticMatrix(fill(Inf, (I,J)))                 # X[i,j]: insertion cost of delivery node L[i] and its associated pickup node at their best position in route R[j]
+    P = ElasticMatrix(fill(((0, 0), (0, 0)), (I,J)))    # P[i,j]: best insertion postion of associated pickup node and the delivery node L[i] in route R[j]
+    ϕ = ones(Int, J)                                    # ϕ[j]  : binary weight for route R[j]
+    # Step 2: Iterate until all open delivery nodes have been inserted into the route
     for _ ∈ I
+        # Step 2.1: Iterate through all open delivery nodes (and the associated pickup nodes) through every possible insertion position in each route
         z = f(s)
         for (i,c) ∈ pairs(L)
             if !isopen(c) continue end
@@ -152,22 +161,27 @@ function greedy!(rng::AbstractRNG, s::Solution; mode::Symbol)
                 nᵖᵗ = d
                 nᵖʰ = nᵖˢ
                 while true
+                    # Step 2.1.1: Insert associated pickup node cᵖ between tail node nᵖᵗ and head node nᵖʰ in route r
                     insertnode!(cᵖ, nᵖᵗ, nᵖʰ, r, s)
                     nᵈˢ = isopt(r) ? C[r.iˢ] : D[r.iˢ]
                     nᵈᵉ = isopt(r) ? C[r.iᵉ] : D[r.iᵉ]
                     nᵈᵗ = d
                     nᵈʰ = nᵈˢ
                     while true
+                        # Step 2.1.1.1: Insert delivery node cᵈ between tail node nᵈᵗ and head node nᵈʰ in route r
                         insertnode!(cᵈ, nᵈᵗ, nᵈʰ, r, s)
+                        # Step 2.1.1.2: Compute the insertion cost
                         z′ = f(s) * (1 + φ * rand(rng, Uniform(-0.2, 0.2)))
                         Δ  = z′ - z
+                        # Step 2.1.1.3: Revise least insertion cost in route r and the corresponding best insertion position in route r
                         if Δ < X[i,j] X[i,j], P[i,j] = Δ, ((nᵖᵗ.iⁿ, nᵖʰ.iⁿ), (nᵈᵗ.iⁿ, nᵈʰ.iⁿ)) end
-                        # Step 2.1.4: Remove customer node c from its position between tail node nᵗ and head node nʰ
+                        # Step 2.1.1.4: Remove delivery node cᵈ from its position between tail node nᵈᵗ and head node nᵈʰ in route r
                         removenode!(cᵈ, nᵈᵗ, nᵈʰ, r, s)
                         if isequal(nᵈᵗ, nᵈᵉ) break end
                         nᵈᵗ = nᵈʰ
                         nᵈʰ = isequal(r.iᵉ, nᵈᵗ.iⁿ) ? D[nᵈᵗ.iʰ] : C[nᵈᵗ.iʰ]
                     end
+                    # Step 2.1.2: Remove associated pickup node cᵖ between tail node nᵖᵗ and head node nᵖʰ in route r
                     removenode!(cᵖ, nᵖᵗ, nᵖʰ, r, s)
                     if isequal(nᵖᵗ, nᵖᵉ) break end
                     nᵖᵗ = nᵖʰ
@@ -175,8 +189,8 @@ function greedy!(rng::AbstractRNG, s::Solution; mode::Symbol)
                 end
             end
         end
+        # Step 2.2: Insert the delivery node and the associated pickup node with least insertion cost at their best position
         i,j= Tuple(argmin(X))
-        Δ = X[i,j]
         c  = L[i]
         cᵖ = isdelivery(c) ? s.C[c.jⁿ] : s.C[c.iⁿ] 
         cᵈ = isdelivery(c) ? s.C[c.iⁿ] : s.C[c.jⁿ]
@@ -195,10 +209,11 @@ function greedy!(rng::AbstractRNG, s::Solution; mode::Symbol)
         insertnode!(cᵈ, nᵈᵗ, nᵈʰ, r, s)
         # Step 2.3: Revise vectors appropriately
         X[i,:] .= Inf
+        P[i,:] .= (((0 ,0), (0, 0)), )
         ϕ .= 0
         for (j,r) ∈ pairs(R) 
             φʳ = isequal(r, c.r)
-            φᵛ = isequal(r.iᵛ, v.iᵛ) && isless(c.r.tⁱ, r.tⁱ) && isequal(φᵉ::Bool, true)
+            φᵛ = isequal(r.iᵛ, v.iᵛ) && isless(c.r.tⁱ, r.tⁱ) && isequal(s.φ, true)
             φᵈ = isequal(r.iᵈ, d.iⁿ) && !hasslack(d)
             φˢ = φʳ || φᵛ || φᵈ
             if isequal(φˢ, false) continue end
@@ -232,17 +247,19 @@ end
 """
     precise!(rng::AbstractRNG, s::Solution)
 
-Returns solution `s` after iteratively inserting customer nodes with 
-least insertion cost until all open customer nodes have been added 
-to the solution. Estimates insertion cost precisely.
+Returns solution `s` after iteratively inserting delivery node 
+and its associated pickup node with least insertion cost until all 
+open delivery nodes have been added to the solution. 
+Estimates insertion cost precisely.
 """
 precise!(rng::AbstractRNG, s::Solution) = greedy!(rng, s; mode=:pcs)
 """
     precise!(rng::AbstractRNG, s::Solution)
 
-Returns solution `s` after iteratively inserting customer nodes with 
-least insertion cost until all open customer nodes have been added to 
-the solution. Estimates insertion cost with a perturbration.
+Returns solution `s` after iteratively inserting delivery node 
+and its associated pickup node with least insertion cost until all 
+open delivery nodes have been added to the solution.  
+Estimates insertion cost with a perturbration.
 """
 perturb!(rng::AbstractRNG, s::Solution) = greedy!(rng, s; mode=:ptb)
 
@@ -251,28 +268,31 @@ perturb!(rng::AbstractRNG, s::Solution) = greedy!(rng, s; mode=:ptb)
 """
     regretk!(rng::AbstractRNG, s::Solution, k̅::Int)
 
-Returns solution `s` after iteratively adding customer nodes with 
-highest regret-k cost at its best position until all open customer 
-nodes have been added to the solution.
+Returns solution `s` after iteratively adding delivery node 
+and its associated pickup node with highest regret-k cost at 
+its best position until all open delivery nodes have been 
+added to the solution.
+Note, regretk mechanism breaks any ties by inserting the 
+node with the lowest insertion cost.
 """
 function regretk!(rng::AbstractRNG, s::Solution, k̅::Int)
     # Step 1: Initialize
     preinsert!(s)
     D = s.D
     C = s.C
-    R = [r for d ∈ D for v ∈ d.V for r ∈ v.R if isactive(r)]
-    L = [c for c ∈ C if isopen(c) && ispickup(c)]
+    R = [r for d ∈ D for v ∈ d.V for r ∈ v.R]
+    L = [c for c ∈ C if isopen(c) && isdelivery(c)]
     I = eachindex(L)
     J = eachindex(R)
-    X = ElasticMatrix(fill(Inf, (I,J)))     # X[i,j]: insertion cost of customer node L[i] at best position in route R[j]
-    P = ElasticMatrix(fill(((0, 0), (0, 0)), (I,J)))  # P[i,j]: best insertion postion of customer node L[i] in route R[j]
-    Y = fill(Inf, (I,k̅))                    # Y[i,k]: insertion cost of customer node L[i] at kᵗʰ best position
-    ϕ = ones(Int, J)                        # ϕ[j]  : binary weight for route R[j]
-    N = zeros(Int, (I,k̅))                   # N[i,k]: route index of customer node L[j] at kᵗʰ best position
-    Z = fill(-Inf, I)                       # Z[i]  : regret-N cost of customer node L[i]
-    # Step 2: Iterate until all open customer nodes have been inserted into the route
+    X = ElasticMatrix(fill(Inf, (I,J)))                 # X[i,j]: insertion cost of delivery node L[i] and its associated pickup node at their best position in route R[j]
+    P = ElasticMatrix(fill(((0, 0), (0, 0)), (I,J)))    # P[i,j]: best insertion postion of associated pickup node and the delivery node L[i] in route R[j]
+    Y = fill(Inf, (I,k̅))                                # Y[i,k]: insertion cost of delivery node L[i] and its associated pickup node at kᵗʰ best position
+    ϕ = ones(Int, J)                                    # ϕ[j]  : binary weight for route R[j]
+    N = zeros(Int, (I,k̅))                               # N[i,k]: route index of delivery node L[i] and its associated pickup node at kᵗʰ best position
+    Z = fill(-Inf, I)                                   # Z[i]  : regret-N cost of delivery node L[i] and its associated pickup node
+    # Step 2: Iterate until all open delivery nodes have been inserted into the route
     for _ ∈ I
-        # Step 2.1: Iterate through all open customer nodes and every route
+        # Step 2.1: Iterate through all open delivery nodes (and the associated pickup nodes)
         z = f(s)
         for (i,c) ∈ pairs(L)
             if !isopen(c) continue end
@@ -287,16 +307,21 @@ function regretk!(rng::AbstractRNG, s::Solution, k̅::Int)
                 nᵖᵗ = d
                 nᵖʰ = nᵖˢ
                 while true
+                    # Step 2.1.1.1: Insert associated pickup node cᵖ between tail node nᵖᵗ and head node nᵖʰ in route r
                     insertnode!(cᵖ, nᵖᵗ, nᵖʰ, r, s)
                     nᵈˢ = isopt(r) ? C[r.iˢ] : D[r.iˢ]
                     nᵈᵉ = isopt(r) ? C[r.iᵉ] : D[r.iᵉ]
                     nᵈᵗ = d
                     nᵈʰ = nᵈˢ
                     while true
+                        # Step 2.1.1.1.1: Insert delivery node cᵈ between tail node nᵈᵗ and head node nᵈʰ in route r
                         insertnode!(cᵈ, nᵈᵗ, nᵈʰ, r, s)
+                        # Step 2.1.1.1.2: Compute the insertion cost
                         z′ = f(s)
                         Δ  = z′ - z
+                        # Step 2.1.1.1.3: Revise least insertion cost in route r and the corresponding best insertion position in route r
                         if Δ < X[i,j] X[i,j], P[i,j] = Δ, ((nᵖᵗ.iⁿ, nᵖʰ.iⁿ), (nᵈᵗ.iⁿ, nᵈʰ.iⁿ)) end
+                        # Step 2.1.1.1.4: Revise N least insertion costs
                         k̲ = 1
                         for k ∈ 1:k̅ 
                             k̲ = k
@@ -306,22 +331,24 @@ function regretk!(rng::AbstractRNG, s::Solution, k̅::Int)
                             Y[i,k] = isequal(k, k̲) ? Δ : Y[i,k-1]
                             N[i,k] = isequal(k, k̲) ? r.iʳ : N[i,k-1]
                         end
+                        # Step 2.1.1.1.5: Remove delivery node cᵈ from its position between tail node nᵈᵗ and head node nᵈʰ in route r
                         removenode!(cᵈ, nᵈᵗ, nᵈʰ, r, s)
                         if isequal(nᵈᵗ, nᵈᵉ) break end
                         nᵈᵗ = nᵈʰ
                         nᵈʰ = isequal(r.iᵉ, nᵈᵗ.iⁿ) ? D[nᵈᵗ.iʰ] : C[nᵈᵗ.iʰ]
                     end
+                    # Step 2.1.1.2: Remove associated pickup node cᵖ between tail node nᵖᵗ and head node nᵖʰ in route r
                     removenode!(cᵖ, nᵖᵗ, nᵖʰ, r, s)
                     if isequal(nᵖᵗ, nᵖᵉ) break end
                     nᵖᵗ = nᵖʰ
                     nᵖʰ = isequal(r.iᵉ, nᵖᵗ.iⁿ) ? D[nᵖᵗ.iʰ] : C[nᵖᵗ.iʰ]
                 end
             end
-            # Step 2.1.2: Compute regret cost for customer node c
+            # Step 2.1.2: Compute regret cost for delivery node L[i]
             Z[i] = 0.
             for k ∈ 1:k̅ Z[i] += Y[i,k] - Y[i,1] end
         end
-        # Step 2.2: Insert customer node with highest regret cost in its best position (break ties by inserting the node with the lowest insertion cost)
+        # Step 2.2: Insert delivery node and the associated pickup node with highest regret cost in its best position (break ties by inserting the nodes with the lowest insertion cost)
         I̲  = findall(isequal.(Z, maximum(Z)))
         i,j= Tuple(argmin(X[I̲,:]))
         i  = I̲[i]
@@ -343,24 +370,25 @@ function regretk!(rng::AbstractRNG, s::Solution, k̅::Int)
         insertnode!(cᵈ, nᵈᵗ, nᵈʰ, r, s)
         # Step 2.3: Revise vectors appropriately
         X[i,:] .= Inf
+        P[i,:] .= (((0 ,0), (0, 0)), )
         Y[i,:] .= Inf
         N[i,:] .= 0
         Z .= -Inf 
         for (i,c) ∈ pairs(L)
             for k ∈ 1:k̅
                 if iszero(N[i,k]) break end
-                k′ = findfirst(r -> isequal(r.iʳ, N[i,k]), R)
-                r  = R[k′]
+                j = findfirst(r -> isequal(r.iʳ, N[i,k]), R)
+                r = R[j]
                 if isequal(r.iᵛ, v.iᵛ) Y[i,k], N[i,k] = Inf, 0 end
             end
-            k′ = sortperm(Y[i,:])
-            Y[i,:] .= Y[i,k′]
-            N[i,:] .= N[i,k′]
+            K = sortperm(Y[i,:])
+            Y[i,:] .= Y[i,K]
+            N[i,:] .= N[i,K]
         end
         ϕ .= 0
         for (j,r) ∈ pairs(R) 
             φʳ = isequal(r, c.r)
-            φᵛ = isequal(r.iᵛ, v.iᵛ) && isless(c.r.tⁱ, r.tⁱ) && isequal(φᵉ::Bool, true)
+            φᵛ = isequal(r.iᵛ, v.iᵛ) && isless(c.r.tⁱ, r.tⁱ) && isequal(s.φ, true)
             φᵈ = isequal(r.iᵈ, d.iⁿ) && !hasslack(d)
             φˢ = φʳ || φᵛ || φᵈ
             if isequal(φˢ, false) continue end
@@ -397,6 +425,8 @@ end
 Returns solution `s` after iteratively adding customer nodes with 
 highest regret-2 cost at its best position until all open customer 
 nodes have been added to the solution.
+Note, regret2 mechanism breaks any ties by inserting the 
+node with the lowest insertion cost.
 """
 regret2!(rng::AbstractRNG, s::Solution) = regretk!(rng, s, 2)
 """
@@ -405,5 +435,7 @@ regret2!(rng::AbstractRNG, s::Solution) = regretk!(rng, s, 2)
 Returns solution `s` after iteratively adding customer nodes with 
 highest regret-3 cost at its best position until all open customer 
 nodes have been added to the solution.
+Note, regret3 mechanism breaks any ties by inserting the 
+node with the lowest insertion cost.
 """
 regret3!(rng::AbstractRNG, s::Solution) = regretk!(rng, s, 3)
